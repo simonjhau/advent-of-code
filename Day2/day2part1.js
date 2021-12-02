f5e3efb49a5403f5416cafa3e284a7c913bc6451
Simon Au <simon@aumail.org>
2021-12-02 20:07:12 +0000

    Day 2

--- Day2/day2part1.js
+++ Day2/day2part1.js
@@ -3,7 +3,26 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const commands = input.split('\n').map((command) => {
   splitCommand = command.split(' ');
-  return { command: splitCommand[0], units: splitCommand[1] };
+  return { direction: splitCommand[0], amount: parseInt(splitCommand[1]) };
 });
 
-console.log(commands[0]);
+let horizontal = 0;
+let depth = 0;
+
+for (command of commands) {
+  switch (command.direction) {
+    case 'forward':
+      horizontal += command.amount;
+      break;
+    case 'up':
+      depth -= command.amount;
+      break;
+    case 'down':
+      depth += command.amount;
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(horizontal * depth);
--- Day2/day2part2.js
+++ Day2/day2part2.js
@@ -1,14 +1,31 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('input.txt', 'utf8');
-const depths = data.split('\n').map((depth) => parseInt(depth));
+const input = fs.readFileSync('input.txt', 'utf8');
+const commands = input.split('\n').map((command) => {
+  splitCommand = command.split(' ');
+  return { direction: splitCommand[0], amount: parseInt(splitCommand[1]) };
+});
 
-let numIncreases = 0;
-for (let i = 0; i < depths.length - 3; i++) {
-  // Optimisation - windowing method of size 3 relies on difference between ith and i+3th values
-  if (depths[i + 3] > depths[i]) {
-    numIncreases++;
+let horizontal = 0;
+let depth = 0;
+let aim = 0;
+
+for (const command of commands) {
+  switch (command.direction) {
+    case 'forward':
+      horizontal += command.amount;
+      depth += aim * command.amount;
+      break;
+    case 'up':
+      aim -= command.amount;
+      break;
+    case 'down':
+      aim += command.amount;
+      break;
+    default:
+      // Case should not happen.  Do nothing
+      break;
   }
 }
 
-console.log(numIncreases);
+console.log(horizontal * depth);
