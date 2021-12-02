59cead0835bee49ffcc5adcc5f18a8892b679782
Simon Au <simon@aumail.org>
2021-12-02 19:33:18 +0000

    Use const

--- Day 1/day1part2.js
+++ Day 1/day1part2.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('input.txt', 'utf8');
-let depths = data.split('\n').map((depth) => parseInt(depth));
+const depths = data.split('\n').map((depth) => parseInt(depth));
 
 let numIncreases = 0;
 for (let i = 0; i < depths.length - 3; i++) {
+  // Optimisation - windowing method of size 3 relies on difference between ith and i+3th values
   if (depths[i + 3] > depths[i]) {
     numIncreases++;
   }
