ce484f8b6f8bacda59b7bf05e6073eb7da863f6e
Simon Au <simon@aumail.org>
2021-12-02 19:11:24 +0000

    fix input

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('inputBinaya.txt', 'utf8', (err, data) => {
+fs.readFile('input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
