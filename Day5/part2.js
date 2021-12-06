d8907d2317cb2579ce4b58ebfeb2991989c34035
Simon Au <simon@aumail.org>
2021-12-06 21:25:31 +0000

    Refactor Day 5

--- Day5/part2.js
+++ Day5/part2.js
@@ -3,51 +3,23 @@ import fs from 'fs';
 const input = fs.readFileSync('input.txt', 'utf8');
 const lines = input
   .split('\n')
-  .map((line) =>
-    line
-      .split(' -> ')
-      .map((coord) => coord.split(',').map((num) => parseInt(num)))
-  );
-
-const start = 0;
-const end = 1;
-const x = 0;
-const y = 1;
+  .map((line) => line.match(/\d+/g).map((num) => parseInt(num)));
 
 const gridSize = 1000; // 1000 for input, 10 for test
 const grid = new Array(gridSize).fill().map(() => new Array(gridSize).fill(0));
 let numWith2Overlaps = 0;
 
-for (const line of lines) {
-  let xDir = 0;
-  if (line[start][x] < line[end][x]) {
-    xDir = 1;
-  } else if (line[start][x] > line[end][x]) {
-    xDir = -1;
-  }
+for (const [x1, y1, x2, y2] of lines) {
+  let xDir = x1 === x2 ? 0 : (x2 - x1) / Math.abs(x2 - x1);
+  let yDir = y1 === y2 ? 0 : (y2 - y1) / Math.abs(y2 - y1);
 
-  let yDir = 0;
-  if (line[start][y] < line[end][y]) {
-    yDir = 1;
-  } else if (line[start][y] > line[end][y]) {
-    yDir = -1;
-  }
+  const lineLength = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1)) + 1;
 
-  const lineLength =
-    Math.max(
-      Math.abs(line[start][x] - line[end][x]),
-      Math.abs(line[start][y] - line[end][y])
-    ) + 1;
-
-  let step = 0;
-  while (step < lineLength) {
-    grid[line[start][y] + step * yDir][line[start][x] + step * xDir] += 1;
-    if (
-      grid[line[start][y] + step * yDir][line[start][x] + step * xDir] === 2
-    ) {
+  for (let step = 0; step < lineLength; step++) {
+    grid[y1 + step * yDir][x1 + step * xDir] += 1;
+    if (grid[y1 + step * yDir][x1 + step * xDir] === 2) {
       numWith2Overlaps++;
     }
-    step++;
   }
 }
 
